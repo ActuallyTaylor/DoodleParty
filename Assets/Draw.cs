a00965f7355df3e387a3a4144353ecc108e5d4be
Zachary Lineman <zachary.lineman@gmail.com>
2019-11-08 22:10:09 -0500

    Zach's Merge

--- Assets/Draw.cs
+++ Assets/Draw.cs
@@ -6,18 +6,25 @@ public class Draw : MonoBehaviour
 {
     private SpriteRenderer sr;
 
+    private Texture2D t;
+
+    private Color black = new Color(0,0,0,1);
+    
+
     // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer> ();
-        Debug.DrawLine(Vector3.zero, new Vector3(5, 0, 0), Color.white, 2.5f);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown (KeyCode.Mouse0)) {
-            
+            Debug.Log("Mouse 0");
+
+            t.SetPixel(Mathf.RoundToInt(Input.mousePosition.x), Mathf.RoundToInt(Input.mousePosition.y), black);
         }
     }
 }
