40cfc69795e7f8aa7e703e5ebc680c5953ddd45b
Zachary Lineman <zachary.lineman@gmail.com>
2019-11-09 00:17:30 -0500

    First Drawing tests

--- Assets/Draw.cs
+++ Assets/Draw.cs
@@ -1,30 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Draw : MonoBehaviour
 {
-    private SpriteRenderer sr;
-
-    private Texture2D t;
-
-    private Color black = new Color(0,0,0,1);
-    
+    public Transform baseDot;
+    private Color black = new Color(0,0,0,1);    
+    private Color white = new Color(1,1,1,1);
+    Texture2D texture;
 
     // Start is called before the first frame update
     void Start()
     {
-        sr = GetComponent<SpriteRenderer> ();
-        
+        var rawImage = GetComponent<RawImage>();
+        texture = rawImage.texture as Texture2D;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown (KeyCode.Mouse0)) {
-            Debug.Log("Mouse 0");
+        Vector2 mousePosition = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-            t.SetPixel(Mathf.RoundToInt(Input.mousePosition.x), Mathf.RoundToInt(Input.mousePosition.y), black);
+        if (Input.GetKey (KeyCode.Mouse0)) {
+            Debug.Log("Yeet");
+            texture.SetPixel((int)objPosition.x, (int)objPosition.y, black);  
+            print(objPosition.x);
+            print(objPosition.y);
         }
+        texture.Apply();       
+
     }
 }
