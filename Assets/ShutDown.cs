ba85ffdf9ac96761bbc6a388ca04a4318da1bebf
SammyIsGod <57544768+SammyIsGod@users.noreply.github.com>
2019-11-09 13:13:02 -0500

    SamIsEpic

---
+++ Assets/ShutDown.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShutDown : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown (0)) {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+            Vector2 mousePos2D = new Vector2 (mousePos.x, mousePos.y);
+
+            RaycastHit2D lit = Physics2D.Raycast (mousePos2D, Vector2.zero);
+            if (lit.collider.tag == "quit" != null) {
+                Debug.Log ("ShuttingDown");
+                Application.Quit();
+            }
+        }
+    }
+}
