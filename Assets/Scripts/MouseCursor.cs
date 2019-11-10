80ee08ef58524eae0db0608ccfaea91005051e3f
Zachary Lineman <zachary.lineman@gmail.com>
2019-11-10 11:18:20 -0500

    Menu UI Setup

--- Assets/Scripts/MouseCursor.cs
+++ Assets/Scripts/MouseCursor.cs
@@ -17,6 +17,8 @@ namespace FreeDraw
         bool onCanvas = false;
         int color = 0;
         Color c = Color.black;
+        public bool click = false;
+        public Animator animator;
     // Start is called before the first frame update
         void Start() {
             sr.color = Color.black;
@@ -25,30 +27,49 @@ namespace FreeDraw
 
         // Update is called once per frame
         void Update() {
+
             Vector3 movement = new Vector3(Input.GetAxis("MoveHorizontal"), Input.GetAxis("MoveVerticle"),0.0f);
             transform.position = transform.position + movement * Time.deltaTime * speed;
 
-            if (Input.GetButton("Draw")) {
-                //Drawing Stuff
-                if (!drew) {
-                    Collider2D hit = Physics2D.OverlapPoint(transform.position + movement * Time.deltaTime, Drawing_Layers.value);
+            if (Input.GetButton("Draw") && !drew) {
+                Collider2D hit = Physics2D.OverlapPoint(transform.position + movement * Time.deltaTime, Drawing_Layers.value);
 
-                    if (hit != null && hit.transform != null) {
-                        DrawArea.BrushTemplate(transform.position + movement * Time.deltaTime);
-                        onCanvas = true;
-                    } else {
-                        onCanvas = false;
-                    }
-                }
-                //UI Stuff
-                Collider2D hit2 = Physics2D.OverlapPoint(transform.position + movement * Time.deltaTime, UI_Layers.value);
-
-                if (hit2 != null && hit2.transform != null) {
-                    print("Yeet");
+                if (hit != null && hit.transform != null) {
+                    DrawArea.BrushTemplate(transform.position + movement * Time.deltaTime);
+                    onCanvas = true;
                 } else {
+                    onCanvas = false;
+                }
 
-                }     
             }
+            Collider2D canvas = Physics2D.OverlapPoint(transform.position + movement * Time.deltaTime, Drawing_Layers.value);
+
+            if (canvas != null && canvas.transform != null) {
+                onCanvas = true;
+                sr.enabled = true;
+            } else {
+                onCanvas = false;
+                sr.enabled = false;    
+            }
+
+            if (Input.GetButtonDown("Draw")) {
+                //Here is wher you want to put all of your UI Stuff
+                Collider2D hitUI = Physics2D.OverlapPoint(transform.position + movement * Time.deltaTime, UI_Layers.value);
+
+                if (hitUI != null && hitUI.transform != null) {
+                    print("Yeet");
+                    click = true;
+
+                } else {
+                    click = false;
+
+                }    
+                
+                animator.SetBool("Click", click);
+                transform.GetChild(0).gameObject.SetActive(click);
+            }
+
+
             if (Input.GetButtonUp("Draw") && onCanvas) {
                 drew = true;
             }
@@ -60,6 +81,7 @@ namespace FreeDraw
 
             if (Input.GetAxis("SizeDown") == 1) {
                 decrease();
+                print(markerSize);
             }
 
             if (Input.GetButtonDown("CycleColor")) {
@@ -84,7 +106,6 @@ namespace FreeDraw
             }
             
         }
-
         public void increase() {
             markerSize += 1;
             if (markerSize <= 7) {
