f1dd4663b0625221cdb5348901a8d1481fbc9b8a
Zachary Lineman <zachary.lineman@gmail.com>
2019-11-12 09:24:20 -0500

    Fixes

--- Assets/Scripts/MouseCursorVoting.cs
+++ Assets/Scripts/MouseCursorVoting.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MouseCursorVoting : MonoBehaviour
 {
     public int speed;
-
+    public TextMeshProUGUI player1Cat;
+    public TextMeshProUGUI player2Cat;
+    public TextMeshProUGUI player3Cat;
+    public TextMeshProUGUI player4Cat;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        player1Cat.text = PlayerPrefs.GetString("Player1Catagory");
+        player2Cat.text = PlayerPrefs.GetString("Player2Catagory");
+        player3Cat.text = PlayerPrefs.GetString("Player3Catagory");
+        player4Cat.text = PlayerPrefs.GetString("Player4Catagory");
+
     }
 
     // Update is called once per frame
@@ -23,25 +32,25 @@ public class MouseCursorVoting : MonoBehaviour
         if (Input.GetButtonDown("Draw")) {
             //Pick Player 1
             PlayerPrefs.SetInt("Player1Score", PlayerPrefs.GetInt("Player1Score") + 1);
-            SceneManager.LoadScene (1);
+            SceneManager.LoadScene (5);
 
         }
         if (Input.GetButtonDown("B")) {
             //Pick Player 2
             PlayerPrefs.SetInt("Player2Score", PlayerPrefs.GetInt("Player2Score") + 1);
-            SceneManager.LoadScene (1);
+            SceneManager.LoadScene (5);
 
         }
         if (Input.GetButtonDown("X")) {
             //Pick Player 3
             PlayerPrefs.SetInt("Player3Score", PlayerPrefs.GetInt("Player3Score") + 1);
-            SceneManager.LoadScene (1);
+            SceneManager.LoadScene (5);
 
         }
         if (Input.GetButtonDown("Y")) {
             //Pick Player 4
             PlayerPrefs.SetInt("Player4Score", PlayerPrefs.GetInt("Player4Score") + 1);
-            SceneManager.LoadScene (1);
+            SceneManager.LoadScene (5);
 
         }
         
