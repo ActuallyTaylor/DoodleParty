d45470b28c60e3940811665b1d884d6461cb5f6e
Zachary Lineman <zachary.lineman@gmail.com>
2019-11-12 10:16:37 -0500

    Yeet

--- Assets/Scripts/CatagoryHandler.cs
+++ Assets/Scripts/CatagoryHandler.cs
@@ -9,8 +9,8 @@ public class CatagoryHandler : MonoBehaviour
 {
     public TextMeshProUGUI catagoryText;
     public TextMeshProUGUI timeText;
-
     public float waitTime = 10.0f;
+    private float wait;
     private string[] catagories = new string[] { "Tree", "House", "Ball", "Horse", "Monkey", "Giraffe","Shark","Dog","Cat","Pencil","Toothbrush","Computer","Pen","Phone","Paintbrush"
     ,"Bed","Pillow","School","God","Devil","Teapot","Garden","Spongebob","Chair","Beach","Furniture","Rollercoaster","Octopus","Fish","Kid","Adult","Dinosaur","camera","airplane",
     "america", "otter","dishes","emoji","tank","Chicken","Diamond","Cupcake","Man","Women","Popcorn","Movies","skeleton","treasure","jellyfish","snowboard","Landscape","Space",
@@ -24,7 +24,7 @@ public class CatagoryHandler : MonoBehaviour
         catagoryText.text = "Player 1 your Catagory is: " + randomString;
         PlayerPrefs.SetString("Player1Catagory",randomString);
         print(PlayerPrefs.GetString("Player1Catagory"));
-
+        wait = waitTime;
     }
 
     // Update is called once per frame
@@ -43,7 +43,7 @@ public class CatagoryHandler : MonoBehaviour
 
             string randomString = catagories[Random.Range (0, catagories.Length)];
             catagoryText.text = "Player " + currentPlayer + " your Catagory is: " + randomString;
-            waitTime = 10.0f;
+            waitTime = wait;
             PlayerPrefs.SetString("Player" + currentPlayer +"Catagory",randomString);
             print(PlayerPrefs.GetString("Player" + currentPlayer +"Catagory"));
         }
--- Assets/Scripts/MouseCursorVoting.cs
+++ Assets/Scripts/MouseCursorVoting.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MouseCursorVoting : MonoBehaviour
 {
     public int speed;
-
+    public TextMeshProUGUI player1;
+    public TextMeshProUGUI player2;
+    public TextMeshProUGUI player3;
+    public TextMeshProUGUI player4;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,31 +21,35 @@ public class MouseCursorVoting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        player1.text = PlayerPrefs.GetString("Player1Catagory");
+        player2.text = PlayerPrefs.GetString("Player2Catagory");
+        player3.text = PlayerPrefs.GetString("Player3Catagory");
+        player4.text = PlayerPrefs.GetString("Player4Catagory");
 
         Vector3 movement = new Vector3 (Input.GetAxis ("MoveHorizontal"), Input.GetAxis ("MoveVerticle"), 0.0f);
         transform.position = transform.position + movement * Time.deltaTime * speed;
         if (Input.GetButtonDown("Draw")) {
             //Pick Player 1
             PlayerPrefs.SetInt("Player1Score", PlayerPrefs.GetInt("Player1Score") + 1);
-            SceneManager.LoadScene (1);
+            SceneManager.LoadScene (5);
 
         }
         if (Input.GetButtonDown("B")) {
             //Pick Player 2
             PlayerPrefs.SetInt("Player2Score", PlayerPrefs.GetInt("Player2Score") + 1);
-            SceneManager.LoadScene (1);
+            SceneManager.LoadScene (5);
 
         }
         if (Input.GetButtonDown("X")) {
             //Pick Player 3
             PlayerPrefs.SetInt("Player3Score", PlayerPrefs.GetInt("Player3Score") + 1);
-            SceneManager.LoadScene (1);
+            SceneManager.LoadScene (5);
 
         }
         if (Input.GetButtonDown("Y")) {
             //Pick Player 4
             PlayerPrefs.SetInt("Player4Score", PlayerPrefs.GetInt("Player4Score") + 1);
-            SceneManager.LoadScene (1);
+            SceneManager.LoadScene (5);
 
         }
         
